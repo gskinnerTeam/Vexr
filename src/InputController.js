385ec9acfcd7cfc0babba76ef7f733eacbc5e1ff
Matthew Willox <matthew@gskinner.com>
2016-10-20 16:12:42 -0600

    Added mouse button up/down to keymap.

--- src/InputController.js
+++ src/InputController.js
@@ -5,36 +5,60 @@ export default class InputController {
 		this.keyMap = {};
 		this.mousePos = new Vector2();
 	}
+
 	bindEvents() {
+		document.addEventListener("mouseup", this.setMouseUp.bind(this));
 		document.addEventListener("mousedown", this.setMouseDown.bind(this));
 		document.addEventListener("mousemove", this.setMousePos.bind(this));
 		onkeydown = onkeyup = this.mapKeys.bind(this);
 	}
+
 	unbindEvents() {
+		document.removeEventListener("mouseup", this.setMouseUp.bind(this));
 		document.removeEventListener("mousedown", this.setMouseDown.bind(this));
 		document.removeEventListener("mousemove", this.setMousePos.bind(this));
 		onkeydown = onkeyup = null;
 	}
+
 	setMousePos(e) {
 		this.mousePos.set(e.pageX, e.pageY);
-		console.log(this.mousePos);
+		//console.log(this.mousePos);
 	}
+
+	setMouseUp(e) {
+		console.log(e)
+		var fakeKey = {
+			key: "mouse" + e.button,
+			type: "keyup"
+		};
+		this.mapKeys(fakeKey)
+	}
+
 	setMouseDown(e) {
-		console.log(e);
+		console.log(e)
+		var fakeKey = {
+			key: "mouse" + e.button,
+			type: "keydown"
+		};
+		this.mapKeys(fakeKey)
 	}
-	mapKeys (e) {
+
+	mapKeys(e) {
 		e = e || event;
 		this.keyMap[e.key] = e.type == 'keydown';
 	}
+
 	keyUp(key) {
 		console.log(key);
 	}
+
 	keyDown(key) {
 		console.log(key);
 	}
-	setKeys () {
-		for(var key in this.keyMap){
-			if(this.keyMap[key]) {
+
+	setKeys() {
+		for (var key in this.keyMap) {
+			if (this.keyMap[key]) {
 				this.keyDown(key);
 			} else {
 				this.keyUp(key);
