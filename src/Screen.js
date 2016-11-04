97662ec8a090e395294764bb0871833a4459a93a
Matthew Willox <skintilla@gmail.com>
2016-11-03 18:42:29 -0600

    Added orientation to Screen

--- src/Screen.js
+++ src/Screen.js
@@ -2,7 +2,7 @@ import Events from "./EventLite";
 import Vector2 from "../Libs/Vexr/Vector2";
 
 var resizeId;
-
+var resizeEvent;
 export default class Screen {
     static get dimensions() {
         return Screen._dimensions;
@@ -10,6 +10,14 @@ export default class Screen {
     static set dimensions(value) {
         if(Screen._dimensions != value) {
             Screen._dimensions = value;
+        }
+    }
+    static get orientation() {
+        return Screen._orientation;
+    }
+    static set orientation(value) {
+        if(Screen._orientation != value) {
+            Screen._orientation = value;
         }
     }
     static get center() {
@@ -44,21 +52,27 @@ export default class Screen {
             Screen._anchorPositions = value;
         }
     }
-    
+
     static resize(e) {
         clearTimeout(resizeId);
+        resizeEvent = e;
         resizeId = setTimeout(Screen.recalculate, Screen.resizeDelay);
     }
 
     static recalculate() {
         Screen.dimensions.set(window.innerWidth, window.innerHeight);
+        if(Screen.dimensions.x > Screen.dimensions.y) {
+            Screen.orientation = "landscape";
+        } else {
+            Screen.orientation = "portrait";
+        }
         for(var anchor in Screen.anchors) {
             if(Screen.anchors.hasOwnProperty(anchor)) {
                 Screen.anchorPositions[anchor].set(Screen.anchors[anchor].x * Screen.dimensions.x, Screen.anchors[anchor].y * Screen.dimensions.y);
             }
         }
 
-        Events.trigger("resize", Screen.dimensions, Screen.anchorPositions);
+        Events.trigger("resize", resizeEvent);
     }
 
     static getAnchor(name) {
@@ -86,6 +100,11 @@ export default class Screen {
         Screen.anchorPositions = {};
         Screen.dimensions = new Vector2(window.innerWidth, window.innerHeight);
         Screen.setAnchor("center", 0.5, 0.5);
+        if(Screen.dimensions.x > Screen.dimensions.y) {
+            Screen.orientation = "landscape";
+        } else {
+            Screen.orientation = "portrait";
+        }
     }
 
 }
