75362a3a9874625ad51efe9d08ff4194a1ee6f13
Matthew Willox <skintilla@gmail.com>
2016-11-20 00:44:31 -0700

    fastest matrix multiply I can do with this configuration

--- src/Matrix4x4.js
+++ src/Matrix4x4.js
@@ -9,43 +9,63 @@ export default class Matrix4 {
 
     setMatrix (array) {
         this.raw = array;
-        this.rawCol1 = [this.raw[0], this.raw[4], this.raw[8], this.raw[12]];
-        this.rawCol2 = [this.raw[1], this.raw[5], this.raw[9], this.raw[13]];
-        this.rawCol3 = [this.raw[2], this.raw[6], this.raw[10], this.raw[14]];
-        this.rawCol4 = [this.raw[3], this.raw[7], this.raw[11], this.raw[15]];
-        this.rawRow1 = [this.raw[0], this.raw[1], this.raw[2], this.raw[3]];
-        this.rawRow2 = [this.raw[4], this.raw[5], this.raw[6], this.raw[7]];
-        this.rawRow3 = [this.raw[8], this.raw[9], this.raw[10], this.raw[11]];
-        this.rawRow4 = [this.raw[12], this.raw[13], this.raw[14], this.raw[15]];
     }
 
-    static dot (a,b) {
-        var dots = a[0] * b[0] + a[1] * b[1] + a[2] * b[2] + a[3] * b[3];
-        return dots;
+    static dot (c1, c2, c3, c4, r1, r2, r3, r4) {
+        return c1 * r1 + c2 * r2 + c3 * r3 + c4 * r4;
     }
 
+    transpose () {
+        var row1 = [this.raw[0], this.raw[1], this.raw[2], this.raw[3]];
+        var row2 = [this.raw[4], this.raw[5], this.raw[6], this.raw[7]];
+        var row3 = [this.raw[8], this.raw[9], this.raw[10], this.raw[11]];
+        var row4 = [this.raw[12], this.raw[13], this.raw[14], this.raw[15]];
+
+        this.raw[0]  = row1[0];
+        this.raw[4]  = row1[1];
+        this.raw[8]  = row1[2];
+        this.raw[12] = row1[3];
+
+        this.raw[1]  = row2[0];
+        this.raw[5]  = row2[1];
+        this.raw[9]  = row2[2];
+        this.raw[13] = row2[3];
+
+        this.raw[2]  = row3[0];
+        this.raw[6]  = row3[1];
+        this.raw[10] = row3[2];
+        this.raw[14] = row3[3];
+
+        this.raw[3]  = row4[0];
+        this.raw[7]  = row4[1];
+        this.raw[11] = row4[2];
+        this.raw[15] = row4[3];
+
+    }
+    
     static multiply(a,b, outputMatrix = new Matrix4()) {
         let matrix = new Float32Array(16);
         //let matrix = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
-        matrix[0] = Matrix4.dot(a.column1, b.row1);
-        matrix[1] = Matrix4.dot(a.column2, b.row1);
-        matrix[2] = Matrix4.dot(a.column3, b.row1);
-        matrix[3] = Matrix4.dot(a.column4, b.row1);
+        matrix[0] = Matrix4.dot(a.raw[0], a.raw[4], a.raw[8], a.raw[12], b.raw[0], b.raw[1], b.raw[2], b.raw[3]);
+        matrix[1] = Matrix4.dot(a.raw[1], a.raw[5], a.raw[9], a.raw[13], b.raw[0], b.raw[1], b.raw[2], b.raw[3]);
+        matrix[2] = Matrix4.dot(a.raw[2], a.raw[6], a.raw[10], a.raw[14], b.raw[0], b.raw[1], b.raw[2], b.raw[3]);
+        matrix[3] = Matrix4.dot(a.raw[3], a.raw[7], a.raw[11], a.raw[15], b.raw[0], b.raw[1], b.raw[2], b.raw[3]);
 
-        matrix[4] = Matrix4.dot(a.column1, b.row2);
-        matrix[5] = Matrix4.dot(a.column2, b.row2);
-        matrix[6] = Matrix4.dot(a.column3, b.row2);
-        matrix[7] = Matrix4.dot(a.column4, b.row2);
+        matrix[4] = Matrix4.dot(a.raw[0], a.raw[4], a.raw[8], a.raw[12], b.raw[4], b.raw[5], b.raw[6], b.raw[7]);
+        matrix[5] = Matrix4.dot(a.raw[1], a.raw[5], a.raw[9], a.raw[13], b.raw[4], b.raw[5], b.raw[6], b.raw[7]);
+        matrix[6] = Matrix4.dot(a.raw[2], a.raw[6], a.raw[10], a.raw[14], b.raw[4], b.raw[5], b.raw[6], b.raw[7]);
+        matrix[7] = Matrix4.dot(a.raw[3], a.raw[7], a.raw[11], a.raw[15], b.raw[4], b.raw[5], b.raw[6], b.raw[7]);
 
-        matrix[8] = Matrix4.dot(a.column1, b.row3);
-        matrix[9] = Matrix4.dot(a.column2, b.row3);
-        matrix[10] = Matrix4.dot(a.column3, b.row3);
-        matrix[11] = Matrix4.dot(a.column4, b.row3);
+        matrix[8] = Matrix4.dot(a.raw[0], a.raw[4], a.raw[8], a.raw[12], b.raw[8], b.raw[9], b.raw[10], b.raw[11]);
+        matrix[9] = Matrix4.dot(a.raw[1], a.raw[5], a.raw[9], a.raw[13], b.raw[8], b.raw[9], b.raw[10], b.raw[11]);
+        matrix[10] = Matrix4.dot(a.raw[2], a.raw[6], a.raw[10], a.raw[14], b.raw[8], b.raw[9], b.raw[10], b.raw[11]);
+        matrix[11] = Matrix4.dot(a.raw[3], a.raw[7], a.raw[11], a.raw[15], b.raw[8], b.raw[9], b.raw[10], b.raw[11]);
 
-        matrix[12] = Matrix4.dot(a.column1, b.row4);
-        matrix[13] = Matrix4.dot(a.column2, b.row4);
-        matrix[14] = Matrix4.dot(a.column3, b.row4);
-        matrix[15] = Matrix4.dot(a.column4, b.row4);
+        matrix[12] = Matrix4.dot(a.raw[0], a.raw[4], a.raw[8], a.raw[12], b.raw[12], b.raw[13], b.raw[14], b.raw[15]);
+        matrix[13] = Matrix4.dot(a.raw[1], a.raw[5], a.raw[9], a.raw[13], b.raw[12], b.raw[13], b.raw[14], b.raw[15]);
+        matrix[14] = Matrix4.dot(a.raw[2], a.raw[6], a.raw[10], a.raw[14], b.raw[12], b.raw[13], b.raw[14], b.raw[15]);
+        matrix[15] = Matrix4.dot(a.raw[3], a.raw[7], a.raw[11], a.raw[15], b.raw[12], b.raw[13], b.raw[14], b.raw[15]);
+        //console.log(outputMatrix);
         outputMatrix.setMatrix(matrix);
         return outputMatrix;
     }
@@ -54,28 +74,4 @@ export default class Matrix4 {
         Matrix4.multiply(this, matrix, this);
     }
 
-    get column1 () {
-        return this.rawCol1;
-    }
-    get column2 () {
-        return this.rawCol2;
-    }
-    get column3 () {
-        return this.rawCol3;
-    }
-    get column4 () {
-        return this.rawCol4;
-    }
-    get row1 () {
-        return this.rawRow1;
-    }
-    get row2 () {
-        return this.rawRow2;
-    }
-    get row3 () {
-        return this.rawRow3;
-    }
-    get row4 () {
-        return this.rawRow4;
-    }
 }
